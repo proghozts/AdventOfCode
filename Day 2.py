2551dfd1a87d8018e7a2a8480177412ccfc1188a
proghozts <32559042+proghozts@users.noreply.github.com>
2025-12-04 13:31:04 +1300

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,42 @@
+with open(r"C:\Users\ericb\Downloads\Day2 input.txt") as f:
+    data = f.read().strip().split(",")
+
+
+def IDCheck(input):
+    total = 0
+    for i in input:
+        a, b = i.split('-')
+        a = int(a)
+        b = int(b)
+        for y in range(a, b+1):
+            s = str(y)
+            l = len(s)
+            if l % 2 == 0:
+                mid = l // 2
+                if s[:mid] == s[mid:]:
+                    total += int(y)
+                else:
+                    pass
+    return total
+
+test = ['11-22']
+print(IDCheck(data))
+
+def IDCheck2(input):
+    total = 0
+    for i in input:
+        a, b = i.split('-')
+        a = int(a)
+        b = int(b)
+        for y in range(a, b+1):
+            s = str(y)
+            l = len(s)
+            for x in range(2, l+1):
+                if l % x == 0:
+                    split = s[:l//x]
+                    if split*x == s:
+                        total += y
+                        break
+    return total       
+                    
+print(IDCheck2(data))
