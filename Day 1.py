df793275bd157c065da5cae04e5695a85332cb7a
proghozts <32559042+proghozts@users.noreply.github.com>
2025-12-04 13:40:59 +1300

    Create Day 1.py

---
+++ Day 1.py
@@ -0,0 +1,24 @@
+filename  = '/Users/ericzha/Downloads/Day 1 Input.txt'
+
+with open(filename) as file:
+    lines = [line.rstrip() for line in file]
+
+def code_crack(list, start = 50):
+    count = 0
+    for i in list:
+        if i[0] == 'R':
+            start += int(i[1:])
+            if start >= 100:
+                count += start // 100
+        else:
+            prev = start
+            start -= int(i[1:])
+            if start <= 0:
+                count += start // -100 + 1
+                if prev == 0:
+                    count -= 1
+        start = start % 100
+
+    return str(count)
+
+print(code_crack(lines))
